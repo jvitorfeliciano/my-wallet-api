baa22fad3d53316f16e3f88ef16e0992ab3ccf4e
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-15 03:50:38 -0300

    refactor: add the name change of the validation schema variable

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const usersCollection = db.collection("users");
 const extractsCollection = db.collection("extracts");
 const sessionsCollection = db.collection("sessions");
 
-const userSchema = Joi.object({
+const signupSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
@@ -32,32 +32,36 @@ const userSchema = Joi.object({
 
 app.post("/sign-up", async (req, res) => {
   const { name, email, password } = req.body;
-  
-  const {error} = userSchema.validate({name, email, password}, {abortEarly:false});
 
-  if(error){
-    const errorMessages = error.details.map((detail)=>detail.message);
-    console.log(errorMessages)
+  const { error } = signupSchema.validate(
+    { name, email, password },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    const errorMessages = error.details.map((detail) => detail.message);
+    console.log(errorMessages);
     return res.status(422).send(errorMessages);
   }
-   
-  try{
-    const users  = await usersCollection.findOne({email});
 
-    if(users){
-        return res.status(409).send({message:"Usuário já cadastrado"});
+  try {
+    const users = await usersCollection.findOne({ email });
+
+    if (users) {
+      return res.status(409).send({ message: "Usuário já cadastrado" });
     }
 
-    const encryptedPassword = bcrypt.hashSync(password,10);
-    await usersCollection.insertOne({name, email, password:encryptedPassword});
-     
-    return res.status(201).send({message:"Usuário cadastrado om sucesso"});
-    
-  }catch(err){
-     return res.status(500).send({error:"Erro do servidor"});
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+    await usersCollection.insertOne({
+      name,
+      email,
+      password: encryptedPassword,
+    });
+
+    return res.status(201).send({ message: "Usuário cadastrado om sucesso" });
+  } catch (err) {
+    return res.status(500).send({ error: "Erro do servidor" });
   }
- 
- 
 });
 
 app.listen(5000);
